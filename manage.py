101eb16a83875b828f705e6faa82023a9e7080e1
Jayan Sarkar <jayansarkar@gmail.com>
2018-10-30 01:55:36 +0530

    fixed blueprint importing

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 import os
 
 from app.main import create_app
+from app import blueprint
 
 app = create_app(os.getenv('RUN_ENV') or 'dev')
 
+app.register_blueprint(blueprint)
 app.app_context().push()
 
 
